f65d692ac2c2ac887a76552e986d43e7fb991d61
Dan <vareladanmarj@gmail.com>
2022-11-29 19:51:28 +0800

    add category controller and views

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,50 @@
+class CategoriesController < ApplicationController
+  before_action :get_category, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @categories = Category.all.order(created_at: :desc)
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new category_params
+
+    if @category.save
+      redirect_to categories_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update category_params
+      redirect_to categories_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @category.destroy
+    redirect_to categories_path
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+  def get_category
+    @category = Category.find params[:id]
+  end
+end
