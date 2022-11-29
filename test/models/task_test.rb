e2535bd23d553f64dcf62466eca6554684837903
Dan <vareladanmarj@gmail.com>
2022-11-29 20:09:10 +0800

    modify test to account for relationships with category

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -2,12 +2,14 @@ require "test_helper"
 
 class TaskTest < ActiveSupport::TestCase
   test "should not save when name is empty" do
-    task = Task.new
+    category = Category.create name: "sample name"
+    task = category.tasks.build description: "sample description"
     assert_not task.save
   end
 
   test "name should be atleast 5 characters long" do
-    task = Task.new name: "aaa"
+    category = Category.create name: "sample name"
+    task = category.tasks.build description: "sample description", name: "aaa"
     assert_not task.save
   end
 end
