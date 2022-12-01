c4f1288583be30a80076d711bafa8fbfc2df7f30
Dan <vareladanmarj@gmail.com>
2022-12-01 19:53:48 +0800

    update task model to include deadline

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,17 +2,33 @@ class Task < ApplicationRecord
   DATE_FORMAT = "%B %e, %Y"
   belongs_to :category
   validates :name, presence: true, length: {minimum: 5}
+  validate :deadline_not_in_the_past
 
   # returns an array of all the tasks grouped into days
-  def self.group_per_day
-    all.order(created_at: :desc).each_with_object({}) do |task, days|
-      day = task.created_at.strftime DATE_FORMAT
+  def self.group_per_deadline
+    all.order(deadline: :desc).each_with_object({}) do |task, days|
+      day = task.deadline.strftime DATE_FORMAT
 
       if days[day]
         days[day].push task
       else
         days[day] = [task]
       end
+    end
+  end
+
+  # returns the tasks for today
+  def self.today
+    where deadline: (Time.now.beginning_of_day..Time.now.end_of_day)
+  end
+
+  private
+
+  def deadline_not_in_the_past
+    return if deadline.blank?
+
+    if deadline < Time.now
+      errors.add(:deadline, "cannot be a date in the past")
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_29_120102) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_01_111849) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_120102) do
     t.bigint "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "deadline"
     t.index ["category_id"], name: "index_tasks_on_category_id"
   end
 
