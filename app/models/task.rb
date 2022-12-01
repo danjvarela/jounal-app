4264ac22eeebb268c9c1927643aecf67bc1d9ead
Dan <vareladanmarj@gmail.com>
2022-12-01 12:50:33 +0800

    change method name

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,7 +4,7 @@ class Task < ApplicationRecord
   validates :name, presence: true, length: {minimum: 5}
 
   # returns an array of all the tasks grouped into days
-  def self.grouped_per_day
+  def self.group_per_day
     all.order(created_at: :desc).each_with_object({}) do |task, days|
       day = task.created_at.strftime DATE_FORMAT
 
