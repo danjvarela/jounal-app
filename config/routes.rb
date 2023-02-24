b9326c107c9d567c0fb7e1016690594ffb1cb619
Dan <vareladanmarj@gmail.com>
2023-02-25 05:40:26 +0800

    fix: add devise error messages and remove forgot password

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   end
   resources :tasks
   put "/tasks/:id/complete", to: "tasks#complete", as: "complete_task"
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions'  }
+  get '/users', to: redirect('/users/sign_up') # fix on devise redirecting to /users on failed signup
   root "tasks#index"
 end
