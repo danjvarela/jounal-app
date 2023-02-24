32db0aceec3fc29c2fb635f24ae44b59988971bd
Dan <vareladanmarj@gmail.com>
2023-02-25 06:23:41 +0800

    chore: update seed to use models instead of factorybot

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,12 @@ if user.blank?
   user = User.create(email: "user@example.com", password: "password")
 end
 
-3.times do
-  category = FactoryBot.create(:category, user: user)
-  3.times { |n| FactoryBot.create(:task, category: category) }
-end
+important = Category.create(name: "IMPORTANT")
+Task.create name: "Learn devise", category: important
+
+school = Category.create(name: "School")
+Task.create name: "Assignment", description: "Accomplish assignments", category: school
+
+chores = Category.create(name: "Chores")
+Task.create name: "Sugar", description: "Buy sugar", category: chores
+Task.create name: "Trash", description: "Take out trash", category: chores
